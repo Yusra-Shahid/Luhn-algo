2798fa6e7bc37dadeb3821851f656c62b76a906e
yusra <yusrashahid111@gmail.com>
2022-11-04 01:29:02 +0500

    still working

--- algo.js
+++ algo.js
@@ -1,6 +1,7 @@
 const prompt = require("prompt-sync")({sigint: true});
-
+let ifEvenDouble = 0
 let N= " ";
+j=0;
 N =prompt("Number : ");
 
 if (N.length==16){
@@ -12,7 +13,21 @@ else if (N.length==13){
 else if(N.length==15){
     console.log("American card")
 }
-
+for(i=0 ;i=N.length-1; i--){
+   //var digit=N.charAt(i);
+   //j= parseInt(digit,10) 
+}
+if(totalSum % 10 === 0){
+    console.log('Valid');
+    return true;
+  } else {
+    console.log('Invalid');
+    return false;
+  }
+// if((ifEvenDouble % 2) === 0){
+//     let doubled = arr[i] * 2;
+// }
+console.log(j)
 // function is_alphaDash(N)
 // {
 //  regexp = /^\d{1,3}$/;
